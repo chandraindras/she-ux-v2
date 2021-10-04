ae34c4d9dd204bb8994b4e1cce55602210774a3b
Imanuel Ronaldo <imanuel.ronaldo@stickearn.com>
2021-10-04 19:57:19 +0700

    showing tick

--- resources/views/v2/export/example_comparison_matrix_pdf.blade.php
+++ resources/views/v2/export/example_comparison_matrix_pdf.blade.php
@@ -1,10 +1,19 @@
+<head>
+    <meta http-equiv="Content-Type" content="charset=utf-8" />
+    <style>
+        * {
+            font-family: "DejaVu Sans Mono", monospace;
+        }
+    </style>
+</head>
 <h1>{{ $projectName }}</h1>
 
 <table>
     @foreach($dataComparison as $comparison)
     <tr>
-        <th>{{ !empty($comparison->comparison_name) ? $comparison->comparison_name : '' }}</th>
-        <th>{{ !empty($comparison->competitor1) ? $comparison->competitor1 : '' }}</th>
+        <th>{{ !empty($comparison->aspect) ? $comparison->aspect : '' }}</th>
+{{--        <th>{{ !empty(($comparison->competitor1)) ? $comparison->competitor1 : '' }}</th>--}}
+        <th>{{ !empty(($comparison->competitor1)) ? $comparison->competitor1 : '' }}</th>
         <th>{{ !empty($comparison->competitor2) ? $comparison->competitor2 : '' }}</th>
         <th>{{ !empty($comparison->competitor3) ? $comparison->competitor3 : '' }}</th>
         <th>{{ !empty($comparison->competitor4) ? $comparison->competitor4 : '' }}</th>
